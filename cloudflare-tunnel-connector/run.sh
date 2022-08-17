18e7f7e3d55d0bc6c2efd4c676e1c5a9cf82fe0b
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-08-17 11:35:38 +0200

    add `--no-autoupdate` flag to cloudflared command

--- cloudflare-tunnel-connector/run.sh
+++ cloudflare-tunnel-connector/run.sh
@@ -6,9 +6,9 @@ TOKEN=$(bashio::config 'token')
 
 # print version
 bashio::log.info "cloudflared version:"
-/bin/cloudflared --version
+/bin/cloudflared --no-autoupdate --version
 
 # run tunnel
 bashio::log.info "starting cloudflared tunnel"
-/bin/cloudflared tunnel run --token $TOKEN
+/bin/cloudflared tunnel run --no-autoupdate --token $TOKEN 
 bashio::log.info "cloudflared shut down"
